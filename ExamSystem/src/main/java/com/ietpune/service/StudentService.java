c715aabb6ed214a020a22c172ca58f5e24ecd2e0
IET <IET@192.168.10.125>
2019-08-02 10:41:13 -0700

    first commit

---
+++ ExamSystem/src/main/java/com/ietpune/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.ietpune.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+	public StudentService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
